4867ac7eaf84d33cd7357e9d3dbe742d8ca4383e
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-07-17 14:33:07 +0900

    :gear:Setting

---
+++ src/main/java/com/sku/hackathon_be/HackathonBeApplication.java
@@ -0,0 +1,13 @@
+package com.sku.hackathon_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HackathonBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sku/hackathon_be/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku.hackathon_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
